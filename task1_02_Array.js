df841054a49e5bd2fb26c69bb70891d289fd0792
Dku4it <dku4it@gmail.com>
2024-07-27 22:26:00 +0300

    Create task1_02_Array.js

---
+++ task1_02_Array.js
@@ -0,0 +1,9 @@
+//  Have the following array ["bmw", "mercedes", "audi", "kia"]
+//  Print to console elements of array
+
+
+const cars1 = ['BMW', 'Mercedes', 'Audi', 'KIA']
+
+console.log( cars1 ) // Way 1
+
+for (const car of cars1) { console.log(car) } // Way 2
