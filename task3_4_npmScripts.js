ac5332c0b61167c634bc84feea0f1660c8f309a3
Dku4it <dku4it@gmail.com>
2024-07-28 20:01:51 +0300

    Delete task3_4_npmScripts.js

--- task3_4_npmScripts.js
+++
@@ -1,8 +0,0 @@
-// Run tasks #2 and #3 using npm scripts (npm run ...)
-
-// Created file package.json
-
-// scripts run at terminal
-// npm run task2  -  task3_2.js
-// npm run task3  -  task3_3_CurrentTime30s.js
-// npm run start  -  task2 & task3
